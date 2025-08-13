26fe99c66600cb6a560bde9b5f640eaacf4d3c54
EMERSON BLANCO <e.blanco017@gmail.com>
2025-08-13 11:05:46 -0500

    test: Se agregan test de validacion con schemas

---
+++ api-tests/src/test/java/com/co/finanzauto/qa/jsonplaceholder/JsonPlaceHolderValidationTests.java
@@ -0,0 +1,39 @@
+package com.co.finanzauto.qa.jsonplaceholder;
+
+import com.co.finanzauto.qa.base.BasePlaceHolder;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.lessThan;
+
+public class JsonPlaceHolderValidationTests extends BasePlaceHolder {
+
+    @Test
+    void getAllPosts_schema() {
+        given()
+                .when()
+                .get("/posts")
+                .then()
+                .statusCode(200)
+                .time(lessThan(2000L))
+                .body(matchesJsonSchemaInClasspath("schemas/posts_schema.json"));
+    }
+
+    @Test
+    void postSchemaValidation() {
+        
+        String payload = """
+            { "title": "foo", "body": "bar", "userId": 1 }
+            """;
+        given()
+                .contentType("application/json")
+                .body(payload)
+                .when()
+                .post("/posts")
+                .then()
+                .statusCode(201)
+                .body(matchesJsonSchemaInClasspath("schemas/post_schema.json"));
+
+    }
+}
